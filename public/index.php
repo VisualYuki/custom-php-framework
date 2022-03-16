39e12dddd1435956fd050ff6a43b064ed77894ae
Moshkin Denis <disant1998@yandex.ru>
2022-03-16 20:08:19 +0300

    28:42 2 lesson

--- public/index.php
+++ public/index.php
@@ -1,23 +1,20 @@
 <?php
-//print_r($_SERVER);
-$query =  rtrim($_SERVER["REQUEST_URI"], "/");
-
 require "../vendors/core/Router.php";
 require "../vendors/libs/functions.php";
+require "../app/controllers/Main.php";
+require "../app/controllers/Posts.php";
 
-Router::add("^s", ["controller" => "Main", "action" => "index"]);
+
+$query = trim($_SERVER["REQUEST_URI"], "/");
+
+echo $query;
+Router::add("^$", ["controller" => "Main", "action" => "index"]);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
 //debug(Router::getRoutes());
 
 Router::dispatch($query);
 
-//if(Router::matchRoute($query)) {
-//    debug(Router::getRoute());
-//}
-//else {
-//    echo "404";
-//}
 
 
 
--- vendors/core/Router.php
+++ vendors/core/Router.php
@@ -5,38 +5,67 @@ class Router
     protected static $routes = [];
     protected static $route = [];
 
-    public static function add($regexp, $route = []) {
+    public static function add($regexp, $route = [])
+    {
         self::$routes[$regexp] = $route;
     }
 
-    public static function getRoutes() {
+    public static function getRoutes()
+    {
         return self::$routes;
     }
 
-    public static function getRoute() {
+    public static function getRoute()
+    {
         return self::$route;
     }
 
-    public static function matchRoute($url) {
+    public static function matchRoute($url)
+    {
+        //echo $url;
+        foreach (self::$routes as $regexp => $route) {
 
-        foreach(self::$routes as $pattern => $route) {
-            if(preg_match("#$pattern#i", $url, $matches)) {
-                debug($matches);
+            if (preg_match("#$regexp#i", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+
+                }
+
+                if (!isset($route["action"])) {
+                    $route["action"] = "index";
+                }
+
                 self::$route = $route;
+                debug(self::$route);
+
                 return true;
             }
-
         }
 
         return false;
     }
 
-    public static function dispatch($url) {
-        if(self::matchRoute($url)){
-            echo "ok";
-        }else {
+    /**
+     * перенапрявляет URL по корректному маршруту
+     * @param string $url входящий URL
+     * @return void
+     */
+    public static function dispatch($url)
+    {
+        if (self::matchRoute($url)) {
+            $controller = self::$route["controller"];
+
+            if(class_exists($controller)) {
+                echo "OK";
+            }
+            else {
+                echo "Контроллер <b>$controller</b> не найден";
+            }
+        } else {
             http_response_code(404);
             include "404.html";
         }
-}
+    }
 }
